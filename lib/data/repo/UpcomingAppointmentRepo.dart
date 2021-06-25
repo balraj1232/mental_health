4af37773732d8377d4ab2d527c242dea4ee537db
Manpreet <manpreet.kaur@softuvo.com>
2021-06-26 00:07:28 +0530

    26-06-2021

---
+++ lib/data/repo/UpcomingAppointmentRepo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health/base/BaseRepository.dart';
+import 'package:mental_health/data/api/ApiEndPoint.dart';
+import 'package:mental_health/models/AppointmentModal.dart';
+import 'package:mental_health/models/sendOtpModel.dart';
+import 'package:dio/dio.dart';
+
+
+class UpcomingAppointmentRepo extends BaseRepository {
+
+  Future<AppointmentModal> pastAppointmentRepo({String therapistId,
+    BuildContext context,
+  }) async {
+    final uri = '${ApiEndpoint.BaseUrl}appointment/upcoming';
+    var response = await Dio().get(uri,
+        queryParameters:
+        {'therapist_id ': therapistId},
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          followRedirects: false,
+        ));
+    try {
+      if (response.data != null) {
+        final passEntity = AppointmentModal.fromJson(response.data);
+        return passEntity;
+      } else {
+        return AppointmentModal(meta: response.data);
+      }
+    } catch (error, stacktrace) {}
+  }
+}
+
